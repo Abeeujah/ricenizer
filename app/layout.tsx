6dc48d386a26278f88c43b01b643f63e6ce7d5d2
Abeeujah <abeeujah@gmail.com>
2024-06-28 03:44:02 +0100

    rm sharp

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,12 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(siteConfig.url.base),
   title: { default: siteConfig.name, template: `%s | ${siteConfig.name}` },
   description: siteConfig.description,
   keywords: siteConfig.keywords,
   authors: [{ name: siteConfig.author, url: siteConfig.url.author }],
   creator: siteConfig.author,
-  metadataBase: new URL(siteConfig.url.base),
   openGraph: {
     type: "website",
     locale: "en_US",
